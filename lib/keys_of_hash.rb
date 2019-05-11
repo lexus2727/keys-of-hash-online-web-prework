2ffae1b2fa306f6ca8602ecb318388d2b2fdfc3c
Candice Parker <lexus2424@gmail.com>
2019-05-11 20:38:16 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,12 +3,13 @@ class Hash
   def keys_of(*arguments)
     #binding.pry
     self.each do |i|
-      binding.pry
+      #binding.pry
       arguments.each do |key, value|
         if i = value
           
     # code goes here
     arguments
+  end
   end
  end 
 end
